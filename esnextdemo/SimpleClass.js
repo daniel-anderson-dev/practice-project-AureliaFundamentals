f302361add5905137633a71064ec113af5fb2b0f
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2017-02-07 17:20:07 -0500

    * Initiated jspm. * Created a sample directory of ES2015/2016 exercises. * Created a JavaScript class and associated object. * Including ALL items into source control - don't want to deal with .gitignore at the moment.

---
+++ esnextdemo/SimpleClass.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class SimpleClass{
+    constructor() {
+        this.name = "Barney";
+    }
+
+    get message() {
+        return "Hello es2015";
+    }
+
+    calculate ()
+    {
+        return 42;
+    }
+}
