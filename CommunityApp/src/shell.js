0ae7226d81a8d55f0738ba606e37054af4dd809f
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2017-02-16 17:45:47 -0500

    * Moved some of the ViewModels into sub-directories. * Setup "advance" routing that builds the navigation menu based on the routing definition. * Created some additional mostly empty ViewModels.

--- CommunityApp/src/shell.js
+++ CommunityApp/src/shell.js
@@ -5,7 +5,9 @@ export class Shell
         this.router = router;
         config.title = 'Browser Title';
         config.map([
-            { route: ['', 'events'], moduleId: 'events'}
+            { route: ['', 'events'], moduleId: 'events/events', name: 'Events', title: 'Events', nav: true }
+            , { route: 'jobs', moduleId: 'jobs/jobs', title: 'Jobs', nav: true }
+            , { route: 'discussion', moduleId: 'discussion/discussion', title: 'Discussion', nav: true }
         ]);
     }
 }
