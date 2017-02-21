f433a3f6b3ef2da65bceba7d26cbffe2c2dd93ef
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2017-02-21 16:30:47 -0500

    Added a demo that shows canDeactivate() and deactivate() life cycles.

--- CommunityApp/src/discussion/discussion.js
+++ CommunityApp/src/discussion/discussion.js
@@ -1,10 +1,53 @@
+function getDiscussionInput()
+{
+    // fake data access
+    return "";
+}
+
+function cloneObject(obj)
+{
+    return JSON.parse(JSON.stringify(obj));
+}
+
 export class Discussion
 {
     activate()
     {
-        let promise = new Promise((resolve, reject) => {
-            setTimeout(_ => resolve(), 3000);
-        });
-        return promise;
+
+        // Old Demo Code
+        // let promise = new Promise((resolve, reject) => {
+        //     setTimeout(_ => resolve(), 3000);
+        // });
+        // return promise;
+
+        this.discussionInput = getDiscussionInput();
+        this.originalInput = cloneObject(this.discussionInput);
+    }
+
+    save()
+    {
+        // simulate save
+        this.originalInput = cloneObject(this.discussionInput);
+    }
+
+    canDeactivate()
+    {
+		if (JSON.stringify(cloneObject(this.discussionInput)) != 
+			JSON.stringify(this.originalInput)) {
+			if (confirm("Unsaved data, are you sure you want to navigate away?")) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return true;
+		}
+    }
+
+    deactivate()
+    {
+        console.log('deactivate called ...');
     }
 }
