17619e9116f88a8ff8979a4cdd0ac2c05e7f0570
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2017-02-21 16:47:02 -0500

    Setup ViewPorts in order to sub-divide the standard routing.

--- CommunityApp/src/shell.js
+++ CommunityApp/src/shell.js
@@ -5,10 +5,19 @@ export class Shell
         this.router = router;
         config.title = 'Browser Title';
         config.map([
-            { route: ['', 'events'], moduleId: 'events/events', name: 'Events', title: 'Events', nav: true }
-            , { route: 'jobs', moduleId: 'jobs/jobs', title: 'Jobs', nav: true }
-            , { route: 'discussion', moduleId: 'discussion/discussion', title: 'Discussion', nav: true }
-            , { route: 'eventDetail/:eventId', moduleId: 'events/eventDetail', name: 'eventDetail' }
+                // Copy-paste
+				{ route: ['', 'events'], 
+					viewPorts: { mainContent: {moduleId: 'events/events'}, 
+					sideBar: {moduleId: 'sideBar/sponsors'}} , 
+					name: 'Events', title: 'Events', nav: true},
+				{ route: 'jobs', viewPorts: { mainContent: {moduleId: 'jobs/jobs'}, 
+					sideBar: { moduleId: 'sideBar/sponsors'}},  
+					title: 'Jobs', nav: true },
+				{ route: 'discussion', viewPorts: { mainContent: {moduleId: 'discussion/discussion'}, 
+					sideBar: { moduleId: 'sideBar/ads'} } , 
+					title: 'Discussion', nav:true },
+				{ route: 'eventDetail/:eventId', viewPorts: { mainContent: { moduleId: 'events/eventDetail'}, 
+					sideBar: { moduleId: 'sideBar/ads'} } , name: 'eventDetail'}
         ]);
     }
 }
