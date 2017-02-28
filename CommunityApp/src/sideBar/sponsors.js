275da6ee0a917d98c1af1c88792f716f270095c3
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2017-02-28 17:08:28 -0500

    Rendering mapped values via repeat.for.

--- CommunityApp/src/sideBar/sponsors.js
+++ CommunityApp/src/sideBar/sponsors.js
@@ -4,6 +4,11 @@ export class Sponsors
     {
         this.message = 'Test Message';
         setTimeout(() => this.message = 'Changed after binding.', 3000);
+        this.mapCollection = new window.Map();
+        this.mapCollection.set('a', 'Alpha');
+        this.mapCollection.set('b', 'Bravo');
+        this.mapCollection.set('c', 'Charlie');
+        this.mapCollection.set('d', 'Delta');
     }
 
     doSomething(passedMessage)
