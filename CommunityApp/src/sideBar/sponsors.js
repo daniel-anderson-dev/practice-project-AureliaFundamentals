82bd9f5a4c1f32901d5d9f4521ce153a270752a2
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2017-02-28 17:19:10 -0500

    Removed "Style" bindings.

--- CommunityApp/src/sideBar/sponsors.js
+++ CommunityApp/src/sideBar/sponsors.js
@@ -9,9 +9,6 @@ export class Sponsors
         this.mapCollection.set('b', 'Bravo');
         this.mapCollection.set('c', 'Charlie');
         this.mapCollection.set('d', 'Delta');
-
-        this.styleString = 'background: red;';
-        this.styleObject = { background: 'green'};
     }
 
     doSomething(passedMessage)
