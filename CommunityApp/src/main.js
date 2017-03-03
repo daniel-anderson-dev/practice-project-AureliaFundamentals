735d66cd5d0c4e6b4f1f643e4954e0fcfb6ad983
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2017-03-03 15:23:15 -0500

    Added a dialog prompt.

--- CommunityApp/src/main.js
+++ CommunityApp/src/main.js
@@ -8,7 +8,10 @@ export function configure(aurelia)
     aurelia.use.instance('apiRoot','http://localhost:27092/');
     aurelia.use.globalResources('common/dateFormat');
 
-    aurelia.use.standardConfiguration().developmentLogging();
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging()
+        .plugin('aurelia-dialog');
     aurelia.start().then(a => a.setRoot('shell'));
 
     aurelia.use.transient('SuperPlugin', PlugIn1);
